f0b5cdbbdd423f81dc527da816b74a37b7c2fe0e
Simon Brewer <brewersi@gmail.com>
2020-07-11 16:54:53 -0600

    R script for asymmetric growth parameters

---
+++ asymmetry_model.R
@@ -0,0 +1,19 @@
+library(ggpubr)
+
+###############################################################################
+
+b = 1
+
+biomassA = 1
+biomassB = seq(1,1000)
+nB = length(biomassB)
+
+propA = rep(NA, nB)
+
+for (i in 1:nB) {
+  propA[i] = biomassA^b / (biomassA^b + biomassB[i]^b)
+}
+
+data.frame(A = propA, biomassB) %>%
+  ggline(x = "A", y = "biomassB", 
+         numeric.x.axis = TRUE)
